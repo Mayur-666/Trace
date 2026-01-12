badb915b13ba04db4101b7669f0ff7bebfe737df
Mayur Sonare <mayursonare18@gmail.com>
2026-01-12 22:10:54 +0530

    fix: added strong typings

--- src/hooks/useFindAlgorithm.ts
+++ src/hooks/useFindAlgorithm.ts
@@ -1,7 +1,8 @@
 import { algorithmIndex } from '@/utils/constants';
+import type { AlgorithmData } from '@/utils/types';
 import { useLocation } from 'react-router-dom';
 
-export default function useFindAlgorithm() {
+export default function useFindAlgorithm(): AlgorithmData {
   const { pathname } = useLocation();
   const path = pathname.split('/');
   const category = path[1];
